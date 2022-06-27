37ef51b1bdcf90218f71bf30033da7393e7b4c43
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-06-27 18:22:36 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header/Header";
+import { TaskList } from "./components/TaskList";
+import "./global/global.css";
+export function App() {
+  return (
+    <>
+      <Header />
+      <TaskList />
+    </>
+  );
+}

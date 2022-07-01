8a913a494fd7855ea76e30365ff6b64dbdc056f0
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-07-01 13:37:38 -0300

    taskList

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-
 import styles from "./TaskList.module.css";
-
 import { FiTrash2, FiCheckSquare, FiPlusCircle } from "react-icons/fi";
+import { TaskEmpty } from "./TaskEmpty";
 import TaskInput from "./TaskInput";
-import Clipboard from "../../Clipboard.png";
+import { TaskItem } from "./TaskItem";
+import { TaskHeader } from "./TaskHeader";
 
 interface Task {
   id: number;
@@ -13,6 +13,7 @@ interface Task {
 }
 
 export function TaskList() {
+
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState("");
 
@@ -44,6 +45,11 @@ export function TaskList() {
   function handleRemoveTask(id: number) {
     setTasks((item) => item.filter((value) => value.id != id));
   }
+  const tasksConcluidas = tasks.reduce(
+    (acc, value) => (value.isComplete ? acc + 1 : acc),
+    0
+  );
+  const totalTasks = tasks.length;
 
   return (
     <section className={styles.container}>
@@ -53,70 +59,16 @@ export function TaskList() {
         value={newTaskTitle}
       />
       <main className={styles.listWrapper}>
-        <div>
-          <div className={styles.conclui}>
-            <span className={styles.criadas}>
-              Tarefas Criadas: <span>{tasks.length}</span>
-            </span>
-            <span className={styles.concluidas}>
-              Concluídas:{" "}
-              <span>
-              {tasks.reduce(
-                (acc, value) => (value.isComplete ? acc + 1 : acc),
-                0
-              )}
-              {tasks.length > 0 ? ` de ${tasks.length}` : ""}
-              </span>
-            </span>
-          </div>
-        </div>
-
+        <TaskHeader tasksConcluidas={tasksConcluidas}  totalTasks={totalTasks}/>
         {tasks.map((item) => (
-          <ul className={styles.list} key={item.id}>
-            <li className={styles.listItem} key={item.id}>
-              <div data-testid="task">
-                <label className={styles["checkbox-container"]}>
-                  <input
-                    type="checkbox"
-                    checked={item.isComplete}
-                    className={styles.checkInput}
-                    onChange={() => handleToggleTaskCompletion(item.id)}
-                  />
-                  <span className="checkmark"></span>
-                </label>
-                <p
-                  style={{
-                    textDecoration: `${
-                      item.isComplete ? "line-through" : "initial"
-                    }`,
-                    color: `${item.isComplete ? "#808080" : "#d9d9d9"}`,
-                  }}
-                >
-                  {item.title}
-                </p>
-              </div>
-              <button
-                type="button"
-                data-testid="remove-task-button"
-                className={styles.removeItem}
-                onClick={() => handleRemoveTask(item.id)}
-              >
-                <FiTrash2 size={16}  color={'#d9d9d9'} />
-              </button>
-            </li>
-          </ul>
+          <TaskItem
+            key={item.id}
+            task={item}
+            handleRemoveTask={handleRemoveTask}
+            handleToggleTaskCompletion={handleToggleTaskCompletion}
+          />
         ))}
-        {tasks.length === 0 && (
-          <div className={styles.taskcontainer}>
-            <div className={styles.taskWrapper}>
-              <div>
-                <img src={Clipboard} alt="vazio" />
-              </div>
-              <p>Você ainda não tem tarefas cadastradas</p>
-              <p>Crie tarefas e organize seus items a fazer </p>
-            </div>
-          </div>
-        )}
+        {tasks.length === 0 && <TaskEmpty />}
       </main>
     </section>
   );
